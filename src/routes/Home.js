1902ec544e8a71a5e45e5483d9facc1ef03c1ca3
beomki69 <beomki69@gmail.com>
2021-10-25 18:18:44 +0900

    feat: Get movies query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
   return <div>home</div>;
 };
 
