96cef25c1b20df8b8a3488743d9fcba473741799
beomki69 <beomki69@gmail.com>
2021-10-27 12:38:53 +0900

    chore: Edit apollo client uri

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: "https://movieql-beomki.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
