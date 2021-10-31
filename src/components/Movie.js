bec321d6b4b3381383330abafcfdafc7ffec2231
beomki69 <beomki69@gmail.com>
2021-10-31 23:08:09 +0900

    chore: Change likeMovie to toggleLikeMovie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const LIKE_MOVIE = gql`
-  mutation likeMovie($id: Int!) {
-    likeMovie(id: $id) @client
+  mutation toggleLikeMovie($id: Int!) {
+    toggleLikeMovie(id: $id) @client
   }
 `;
 
@@ -14,14 +14,14 @@ const Img = styled.img`
 `;
 
 const Movie = ({ id, medium_cover_image, title, isLiked }) => {
-  const [likeMovie] = useMutation(LIKE_MOVIE, { variables: { id } });
+  const [toggleLikeMovie] = useMutation(LIKE_MOVIE, { variables: { id } });
 
   return (
     <div>
       <Link to={`/${id}`}>
         <Img src={medium_cover_image} alt={title} />
       </Link>
-      <button onClick={likeMovie}>{isLiked ? "Unlike" : "Like"}</button>
+      <button onClick={toggleLikeMovie}>{isLiked ? "Unlike" : "Like"}</button>
     </div>
   );
 };
